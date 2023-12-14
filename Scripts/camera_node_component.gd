526431688590c2c0ef141206ba2ff505d04e700e
Spiderpat12 <112583095+Spiderpat12@users.noreply.github.com>
2023-12-13 22:24:02 -0800

    CameraComponent

---
+++ Scripts/camera_node_component.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@export var cameraNode : Camera2D
+@export var Target : Node2D
+@export var followSpeed : float
+
+
+
+func _physics_process(delta):
+	global_position = global_position.lerp(Target.global_position, followSpeed * delta)
