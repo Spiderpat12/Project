9bdb41b88cd753ed8171b9e3adf022d953877488
Spiderpat12 <112583095+Spiderpat12@users.noreply.github.com>
2023-12-14 15:39:26 -0800

    acceleration

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,23 +3,30 @@ extends CharacterBody2D
 @export var speed : float
 @export var jump : float
 @export var gravit : float
+@export var acceleration : float
 
 
 func _physics_process(delta):
 	Gravity(delta)
-	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = -jump
-
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-
-	move_and_slide()
+	MoveMent(delta)
+	Jump()
 
 
 func Gravity(delta) -> void:
 	if (!is_on_floor()):
 		velocity.y += gravit
+
+func MoveMent(delta) -> void:
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * speed, acceleration * delta)
+		$Sprite2D.flip_h = direction < 0
+	else:
+		velocity.x = move_toward(velocity.x, 0, acceleration * delta)
+
+	move_and_slide()
+
+
+func Jump() -> void:
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = -jump
