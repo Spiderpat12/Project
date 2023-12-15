8d9fba09dc3ef4d46a4108c67d41da426d654cb5
Spiderpat12 <112583095+Spiderpat12@users.noreply.github.com>
2023-12-14 20:08:37 -0800

    chapter mic

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,18 +1,26 @@
 extends CharacterBody2D
 
-var coyoteTimer = 0.3
-var CanJump = false
+var coyoteTimer : float = 0.3
+var CanJump : bool = false
+var DashValue : float
+var divMul : int = 10000 #دي يا ريان علشان الداش راح نضربه في الاسبيد فا دي هي القيمه
 
 @export var speed : float
 @export var jump : float
 @export var gravit : float
 @export var acceleration : float
+@export_enum("Chapter1","Chapter2","Chapter3","Chapter4") var Chapters
 
+func _ready():
+	DashValue = speed * divMul
 
 func _physics_process(delta):
 	Gravity(delta)
 	MoveMent(delta)
 	Jump()
+	Chapter(delta)
+	print(speed)
+
 
 
 func Gravity(delta) -> void:
@@ -41,6 +49,26 @@ func Jump() -> void:
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = -jump
 
-
 func _on_coyote_timer_timeout():
 	CanJump = false
+
+func Chapter(delta) -> void:
+	match Chapters:
+		0:
+			DashFunciton(delta)
+		1:
+			print("element2")
+		2:
+			print("element3")
+		3:
+			print("element4")
+
+
+func  DashFunciton(delta) -> void:
+	if Input.is_action_just_pressed("DashButton"):
+		speed = DashValue
+		$Timer_Node/DashTimer.start()
+
+
+func _on_dash_timer_timeout():
+	speed = DashValue / divMul
